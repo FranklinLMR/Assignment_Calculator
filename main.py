7e6519873d5245c9c0b4157da9fa1fb1adbd37ac
icemadeofguava <marcosnoeldeleon@gmail.com>
2026-01-28 13:25:13 -0400

    Multiplication and Division

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ def suma(a,b):
 def resta(a,b):
     return a-b
 
-#Multiplicacion aca
+def multi(a,b):
+    return a*b
 
-#Division aca
+def division(a,b):
+    return a/b
 
 
 print("Hello guy, let's keep this simple")
 sleep(2)
 AnswerDecision= input("Do you want to do:\n1. Sum\n2. Substract\n")
+
 match AnswerDecision:
     case "1":
         print("Great")
@@ -36,6 +39,26 @@ match AnswerDecision:
         print("\n\n")
         sleep(1)
         print(f"Here is the result of the substraction {a} - {b}: {resta(a,b)}") 
+
+    case "3":
+        print("Great")
+        sleep(1)
+        a= int(input("Give me the first number: "))
+        print("\n\n")
+        b= int(input("Give me the second number: "))
+        print("\n\n")
+        sleep(1)
+        print(f"Here is the result of the multiplication {a} * {b}: {multi(a,b)}")
+
+    case "4":
+        print("Great")
+        sleep(1)
+        a= int(input("Give me the first number: "))
+        print("\n\n")
+        b= int(input("Give me the second number: "))
+        print("\n\n")
+        sleep(1)
+        print(f"Here is the result of the division {a} / {b}: {division(a,b)}") 
 
     case _:
         pass
