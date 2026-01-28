7bc9052f6a096017e38aad21f011fffc9f49a491
Franklin <rosariosanchezfranklin@gmail.com>
2026-01-28 12:56:38 -0400

    Sum and substraction

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import math
 from time import sleep
 
+
+def suma(a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+
+#Multiplicacion aca
+
+#Division aca
+
+
 print("Hello guy, let's keep this simple")
 sleep(2)
-AnswerDecision= input("Do you want to do:\n1. Sum\n2. Substract")
+AnswerDecision= input("Do you want to do:\n1. Sum\n2. Substract\n")
 match AnswerDecision:
     case "1":
         print("Great")
         sleep(1)
-        n1= int(input("Give me the first number: "))
+        a= int(input("Give me the first number: "))
         print("\n\n")
-        n2= int(input("Give me the second number: "))
+        b= int(input("Give me the second number: "))
 
-        print(f"Here is the result of the sum: {n1+n2}")
+        print(f"Here is the result of the sum: {suma(a,b)}")
 
     case "2":
         print("Great")
         sleep(1)
-        n1= int(input("Give me the first number: "))
+        a= int(input("Give me the first number: "))
         print("\n\n")
-        n2= int(input("Give me the second number: "))
+        b= int(input("Give me the second number: "))
 
-        print(f"Here is the result of the substraction: {n1-n2}") 
+        print(f"Here is the result of the substraction: {resta(a,b)}") 
 
     case _:
         pass
