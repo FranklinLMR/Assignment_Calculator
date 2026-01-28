206317feb5c2a3c1200b2a672cc71319bf3a9b54
Franklin <rosariosanchezfranklin@gmail.com>
2026-01-28 13:33:52 -0400

    Long life to arenque 🐟

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-from time import sleep
+from time import sleep as arenque
 
 
 def suma(a,b):
@@ -16,48 +15,48 @@ def division(a,b):
 
 
 print("Hello guy, let's keep this simple")
-sleep(2)
+arenque(2)
 AnswerDecision= input("Do you want to do:\n1. Sum\n2. Substract\n")
 
 match AnswerDecision:
     case "1":
         print("Great")
-        sleep(1)
+        arenque(1)
         a= int(input("Give me the first number: "))
         print("\n\n")
         b= int(input("Give me the second number: "))
         print("\n\n")
-        sleep(1)
+        arenque(1)
         print(f"Here is the result of the sum {a} + {b}: {suma(a,b)}")
 
     case "2":
         print("Great")
-        sleep(1)
+        arenque(1)
         a= int(input("Give me the first number: "))
         print("\n\n")
         b= int(input("Give me the second number: "))
         print("\n\n")
-        sleep(1)
+        arenque(1)
         print(f"Here is the result of the substraction {a} - {b}: {resta(a,b)}") 
 
     case "3":
         print("Great")
-        sleep(1)
+        arenque(1)
         a= int(input("Give me the first number: "))
         print("\n\n")
         b= int(input("Give me the second number: "))
         print("\n\n")
-        sleep(1)
+        arenque(1)
         print(f"Here is the result of the multiplication {a} * {b}: {multi(a,b)}")
 
     case "4":
         print("Great")
-        sleep(1)
+        arenque(1)
         a= int(input("Give me the first number: "))
         print("\n\n")
         b= int(input("Give me the second number: "))
         print("\n\n")
-        sleep(1)
+        arenque(1)
         print(f"Here is the result of the division {a} / {b}: {division(a,b)}") 
 
     case _:
